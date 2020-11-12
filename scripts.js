bce0a7321197f982f37fbd51ebddfbc7ac05deda
Sherwin Zhang <sherwinzhang100@gmail.com>
2020-11-12 15:42:37 -0800

    note duration and staff selection added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
-function insertNote(note) {
-    document.getElementById("notes").innerHTML = note;
+function setNotePitch(note) {
+    document.getElementById("notePitch").innerHTML = note;
+}
+function setNoteDuration(note) {
+    document.getElementById("noteDuration").innerHTML = note;
+}
+function setStaff(staff) {
+    document.getElementById("staff").innerHTML = staff;
 }
