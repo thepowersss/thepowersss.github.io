dceec81f9fde159e7451b0dc1acaa7cc1daffd94
Sherwin Zhang <sherwinzhang100@gmail.com>
2021-03-15 17:02:36 -0700

    cheese picker attempt 4

--- scripts/generate_cheese.js
+++ scripts/generate_cheese.js
@@ -1,20 +1,15 @@
 function generateCheese() {
-    document.getElementById('/scripts/sc2_cheese_list.txt').addEventListener('change', function selectedFileChanged() {
-        if (this.files.length === 0) {
-            console.log('No file selected.');
-            return;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var textByLine = this.responseText.split("\n");
+            var pick = Math.floor(Math.random() * textByLine.length);
+            document.getElementById("cheese_display").innerHTML = textByLine[pick];
+            //document.getElementById("demo").innerHTML = this.responseText;
         }
-
-        const reader = new FileReader();
-        reader.onload = function fileReadCompleted() {
-            // when the reader is done, the content is in reader.result.
-            console.log(reader.result);
-        };
-        reader.readAsText(this.files[0]);
-        var textByLine = reader.result.split("\n");
-        var pick = Math.floor(Math.random() * textByLine.length);
-        document.getElementById("cheese_display").innerHTML = textByLine[pick];
-    });
+    };
+    xhttp.open("GET", "/scripts/sc2_cheese_list.txt", true);
+    xhttp.send();
 }
     /* doesn't work because front-end cannot see local files
     const fs = require('fs');
